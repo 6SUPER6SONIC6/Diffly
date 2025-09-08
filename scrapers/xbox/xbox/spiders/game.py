f794d6b723dd00cc550673b5f9c75cda7316ea3c
6SUPER6SONIC6 <vadym.tantsiura@gmail.com>
2025-09-08 17:30:54 +0300

    refactor(spider): improve GameSpider - Add error handling - Use .get() accessors to avoid KeyErrors - Make max_pages configurable via spider argument

--- scrapers/xbox/xbox/spiders/game.py
+++ scrapers/xbox/xbox/spiders/game.py
@@ -11,9 +11,10 @@ class GameSpider(scrapy.Spider):
     allowed_domains = ["www.xbox.com", "xboxservices.com"]
     start_urls = ["https://www.xbox.com/en-US/games/browse"]
 
-    def __init__(self, *args, **kwargs):
+    def __init__(self, max_pages=3, *args, **kwargs):
         super(GameSpider, self).__init__(*args, **kwargs)
 
+        self.max_pages = int(max_pages)
         self.api_url = "https://emerald.xboxservices.com/xboxcomfd/browse?locale=en-US"
         self.cv_base = "DSK6KO20k6Y7NXCBkdtipF"
         self.cv_counter = 1
@@ -26,21 +27,29 @@ class GameSpider(scrapy.Spider):
         script_pattern = r'window\.__PRELOADED_STATE__ = ({.*?});'
         match = re.search(script_pattern, response.text, re.DOTALL)
 
-        preloaded_data = json.loads(match.group(1))
+        if not match:
+            self.logger.warning("Could not find preloaded state")
+            return
 
-        games = preloaded_data['core2']['products']['productSummaries']
-        channel_data = preloaded_data['core2']['channels']['channelData']
+        try:
+            preloaded_data = json.loads(match.group(1))
+        except json.decoder.JSONDecodeError as e:
+            self.logger.error(f"Error decoding preloaded state: {e}")
+            return
+
+        products = preloaded_data.get('core2', {}).get('channels', {}).get('productSummaries', {})
+        channel_data = preloaded_data.get('core2', {}).get('channels', {}).get('channelData', {})
 
         channel_key = [k for k in channel_data.keys() if 'BROWSE_CHANNELID' in k][0]
-        game_ids = channel_data[channel_key]['data']['products']
+        game_ids = channel_data.get(channel_key, {}).get('data', {}).get('products', [])
 
         for game_info in game_ids:
-            game_id = game_info['productId']
-            if game_id in games:
-                game_data = games[game_id]
+            game_id = game_info.get('productId')
+            if game_id and game_id in products:
+                game_data = products[game_id]
                 yield self.parse_item(game_data)
 
-        continuation_token = channel_data[channel_key]['data']['encodedCT']
+        continuation_token = channel_data.get(channel_key, {}).get('data', {}).get('encodedCT')
         if continuation_token:
             yield self.create_api_request(continuation_token)
 
@@ -49,15 +58,6 @@ class GameSpider(scrapy.Spider):
         ms_cv = f"{self.cv_base}.{self.cv_counter}"
         self.cv_counter += 1
 
-        headers = {
-            'Accept': '*/*',
-            'Accept-Language': 'en-US,en;q=0.9',
-            'Content-Type': 'application/json',
-            'MS-CV': ms_cv,
-            'x-ms-api-version': '1.1',
-            'Referer': 'https://www.xbox.com/',
-            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/139.0.0.0 Safari/537.36'
-        }
         body = {
             'Filters': 'e30=',
             'ReturnFilters': False,
@@ -65,12 +65,12 @@ class GameSpider(scrapy.Spider):
             'EncodedCT': continuation_token,
             'ChannelId': ''
         }
-        # Loading 3 pages for testing proposes
-        if self.cv_counter <= 3:
+
+        if self.cv_counter <= self.max_pages:
             return scrapy.Request(
-                url='https://emerald.xboxservices.com/xboxcomfd/browse?locale=en-US',
+                url=self.api_url,
                 method='POST',
-                headers=headers,
+                headers={'MS-CV': ms_cv},
                 body=json.dumps(body),
                 callback=self.parse_api_response,
             )
@@ -80,18 +80,18 @@ class GameSpider(scrapy.Spider):
     def parse_api_response(self, response):
         try:
             data = json.loads(response.text)
-        except json.decoder.JSONDecodeError as e:
+        except json.JSONDecodeError as e:
             self.logger.error(f"Error parsing API response: {e}")
             return
 
-        games = data['productSummaries']
-        channel_data = data['channels']
+        games = data.get('productSummaries', [])
+        channel_data = data.get('channels', {})
         channel_key = [k for k in channel_data.keys() if 'BROWSE_CHANNELID' in k][0]
 
         for game_data in games:
             yield self.parse_item(game_data)
 
-        next_continuation_token = channel_data[channel_key]['encodedCT']
+        next_continuation_token = channel_data.get(channel_key, {}).get('encodedCT')
         if next_continuation_token:
             yield self.create_api_request(next_continuation_token)
 
@@ -112,5 +112,5 @@ class GameSpider(scrapy.Spider):
             item['price_base'] = price_data.get('msrp')
             item['price_current'] = price_data.get('listPrice')
 
-        self.logger.info(f"Parsed item: {item['game_title']}")
+        self.logger.info(f"Parsed item: {item.get('game_title')}")
         return item
