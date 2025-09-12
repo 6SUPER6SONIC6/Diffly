5173859a46fbb7b922e03b7407cbb8a22873c749
6SUPER6SONIC6 <vadym.tantsiura@gmail.com>
2025-09-12 15:28:01 +0300

    refactor(Django admin): add Game - Create GameAdmin - Create Price and GameImage inlines

--- apps/games/admin.py
+++ apps/games/admin.py
@@ -1,6 +1,21 @@
 from django.contrib import admin
 
-from .models import Platform, Region
+from .models import Platform, Region, Price, GameImage, Game
 
+
+class PriceInline(admin.TabularInline):
+    model = Price
+    extra = 0
+
+class GameImageInline(admin.TabularInline):
+    model = GameImage
+    extra = 0
+
+class GameAdmin(admin.ModelAdmin):
+    inlines = [PriceInline, GameImageInline]
+    list_filter = ('title',)
+    search_fields = ('title',)
+
+admin.site.register(Game, GameAdmin)
 admin.site.register(Platform)
 admin.site.register(Region)
