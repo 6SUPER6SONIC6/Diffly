47c200613309480383b806f1c6843dfa42c5af4d
6SUPER6SONIC6 <vadym.tantsiura@gmail.com>
2025-09-12 15:26:06 +0300

    refactor(pipeline): improve DjangoModelPipeline - Add error handling - Use update_or_create() for Game - Add stats tracking per region

--- scrapers/xbox/xbox/pipelines.py
+++ scrapers/xbox/xbox/pipelines.py
@@ -1,73 +1,68 @@
-# Define your item pipelines here
-#
-# Don't forget to add your pipeline to the ITEM_PIPELINES setting
-# See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
+from collections import defaultdict
 
 from asgiref.sync import sync_to_async
 from django.db import close_old_connections
 from django.utils import timezone
 from django.utils.dateparse import parse_datetime
+from scrapy.exceptions import DropItem
 
-# useful for handling different item types with a single interface
 from apps.games.models import *
+
+REGION_MAP = {
+    "en-US": "US",
+    "tr-TR": "TR",
+}
 
 
 class DjangoModelPipeline:
+    def __init__(self):
+        self.stats = defaultdict(
+            lambda: {
+                'games': {'crt': 0, 'upd': 0},
+                'prices': {'crt': 0, 'upd': 0},
+                'images': 0
+            }
+        )
 
     async def process_item(self, item, spider):
-        spider.logger.info(
-            f'Saving item: {item.get("product_id")}({item.get('game_title')}) from region {item.get("region")}')
-        platform = await sync_to_async(Platform.objects.get)(name='Xbox')
-        game, _ = await sync_to_async(Game.objects.get_or_create)(product_id=item['product_id'])
+        product_id = item.get('product_id')
+        region = item.get('region')
+        region_code = REGION_MAP.get(region)
+        if not region_code:
+            raise DropItem(f"Invalid region code {region} - {product_id}")
 
-        if item.get('region') == 'en-US':
-            try:
-                release_date = parse_datetime(item.get('game_release_date'))
-            except TypeError:
-                release_date = None
+        # spider.logger.info(f"Saving item: {product_id}({item.get('game_title')}) from {region}")
 
-            await sync_to_async(Game.objects.filter(product_id=item['product_id']).update)(
-                title=item.get('game_title'),
-                description=item.get('game_description', ''),
-                short_description=item.get('game_short_description', ''),
-                developer_name=item.get('game_developer_name', ''),
-                publisher_name=item.get('game_publisher_name', ''),
-                release_date=release_date,
+        release_date = parse_datetime(item.get('game_release_date')) if item.get('game_release_date') else None
+
+        try:
+            # Update game's metadata for the US region only
+            game, created = await sync_to_async(Game.objects.update_or_create)(
+                product_id=product_id,
+                defaults={
+                    'title': item.get('game_title'),
+                    'description': item.get('game_description'),
+                    'short_description': item.get('game_description'),
+                    'developer_name': item.get('game_developer_name'),
+                    'publisher_name': item.get('game_publisher_name'),
+                    'release_date': release_date,
+                } if region == 'en-US' else None,
             )
+        except Exception as e:
+            raise DropItem(f"Failed to create/get game {product_id}: {e}")
+        else:
+            if created:
+                self.stats[region]['games']['crt'] += 1
+            else:
+                self.stats[region]['games']['upd'] += 1
 
-        game_platform, _ = await sync_to_async(GamePlatform.objects.get_or_create)(
+        platform = await sync_to_async(Platform.objects.get)(name='Xbox')
+        await sync_to_async(GamePlatform.objects.get_or_create)(
             platform=platform,
             game=game,
         )
 
-        if not item.get('price_base') or not item.get('price_current'):
-            spider.logger.warning(f'No price for {item.get("product_id")} in {item.get("region")}')
-
-        if "price_base" in item and "region" in item:
-            region_code = item['region'][3:]
-            region = await sync_to_async(Region.objects.get)(code=region_code)
-            store, created = await sync_to_async(Store.objects.get_or_create)(
-                name=f'Xbox Store {region_code}',
-                defaults={
-                    'base_url': f'https://www.xbox.com/{item.get("region")}',
-                }
-            )
-            if created:
-                store.platforms.add(platform)
-
-            price, _ = await sync_to_async(Price.objects.update_or_create)(
-                game=game,
-                platform=platform,
-                region=region,
-                store=store,
-                defaults={
-                    'base_price': item.get('price_base'),
-                    'current_price': item.get('price_current'),
-                    'last_updated': timezone.now(),
-                }
-            )
-
-        if 'images' in item and item['images']:
+        if 'images' in item and item['images'] and region == 'en-US':
             for image_type, data in item.get('images', {}).items():
                 match image_type:
                     case 'boxArt':
@@ -82,17 +77,65 @@ class DjangoModelPipeline:
                         image_type = 'logo'
                     case _:
                         continue
-                game_image, _ = await sync_to_async(GameImage.objects.update_or_create)(
+                try:
+                    await sync_to_async(GameImage.objects.update_or_create)(
+                        game=game,
+                        image_type=image_type,
+                        defaults={
+                            'url': data['url'],
+                            'width': data['width'],
+                            'height': data['height'],
+                        }
+                    )
+                except Exception as e:
+                    spider.logger.error(f"Failed to create/update image for {product_id}: {e}")
+                else:
+                    self.stats[region]['images'] += 1
+
+        if "price_base" in item and "region" in item:
+            price_region = await sync_to_async(Region.objects.get)(code=region_code)
+            store, created = await sync_to_async(Store.objects.get_or_create)(
+                name=f'Xbox Store {region_code}',
+                defaults={
+                    'base_url': f'https://www.xbox.com/{region}',
+                }
+            )
+            if created:
+                store.platforms.add(platform)
+
+            try:
+                price, created = await sync_to_async(Price.objects.update_or_create)(
                     game=game,
-                    image_type=image_type,
+                    platform=platform,
+                    region=price_region,
+                    store=store,
                     defaults={
-                        'url': data['url'],
-                        'width': data['width'],
-                        'height': data['height'],
+                        'base_price': item.get('price_base'),
+                        'current_price': item.get('price_current'),
+                        'last_updated': timezone.now(),
                     }
                 )
+            except Exception as e:
+                spider.logger.error(f"Failed to create/update price for {product_id}: {e}")
+            else:
+                if created:
+                    self.stats[region]['prices']['crt'] += 1
+                else:
+                    self.stats[region]['prices']['upd'] += 1
 
         return item
 
     def close_spider(self, spider):
+        for region, data in self.stats.items():
+            spider.logger.info(
+                f'''
+                ---------------{region}---------------
+                Games created: {data['games']['crt']}
+                Games updated: {data['games']['upd']}
+                Prices created: {data['prices']['crt']}
+                Prices updated: {data['prices']['upd']}
+                Images: {data['images']}
+                ---------------------------------------
+                '''
+            )
         close_old_connections()
