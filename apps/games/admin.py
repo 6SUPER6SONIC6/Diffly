30e867bed5364e9803a387e535a574671d9faaf7
Vadym Tantsiura <vadym.tantsiura@gmail.com>
2025-09-13 18:02:39 +0300

    Revert "feat: Add Django management command for running scrapers (#2)"
    This reverts commit ef64857865849e86227f9b740053fece22750fa6.

--- apps/games/admin.py
+++ apps/games/admin.py
@@ -1,21 +1,3 @@
 from django.contrib import admin
 
-from .models import Platform, Region, Price, GameImage, Game
-
-
-class PriceInline(admin.TabularInline):
-    model = Price
-    extra = 0
-
-class GameImageInline(admin.TabularInline):
-    model = GameImage
-    extra = 0
-
-class GameAdmin(admin.ModelAdmin):
-    inlines = [PriceInline, GameImageInline]
-    list_filter = ('title',)
-    search_fields = ('title', 'product_id')
-
-admin.site.register(Game, GameAdmin)
-admin.site.register(Platform)
-admin.site.register(Region)
+# Register your models here.
--- scrapers/base.py
+++
@@ -1,9 +0,0 @@
-from abc import ABC, abstractmethod
-
-class BaseScraper(ABC):
-    """Abstract base class for scrapers."""
-
-    @abstractmethod
-    def run(self, **kwargs):
-        """Abstract method to run the scraper."""
-        pass
