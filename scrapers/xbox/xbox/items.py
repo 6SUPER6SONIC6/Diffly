b0f90789cbe6bcea87b50b818d06e6dad469ecbb
Vadym Tantsiura <vadym.tantsiura@gmail.com>
2025-09-13 18:04:23 +0300

    Revert "feat: Add Django management command for running scrapers (#2)" (#3)
    This reverts commit ef64857865849e86227f9b740053fece22750fa6.

--- scrapers/xbox/xbox/items.py
+++ scrapers/xbox/xbox/items.py
@@ -7,18 +7,6 @@ import scrapy
 
 
 class XboxItem(scrapy.Item):
-    game_title = scrapy.Field()
-    game_description = scrapy.Field()
-    game_short_description = scrapy.Field()
-    game_developer_name = scrapy.Field()
-    game_publisher_name = scrapy.Field()
-    game_release_date = scrapy.Field()
-    product_id = scrapy.Field()
-
-    images = scrapy.Field()
-
-    price_base = scrapy.Field()
-    price_current = scrapy.Field()
-
-    region = scrapy.Field()
-
+    # define the fields for your item here like:
+    # name = scrapy.Field()
+    pass
