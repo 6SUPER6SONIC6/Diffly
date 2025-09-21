2eb32e53dff77b5dc0b274eb92aca25c51d4a36d
6SUPER6SONIC6 <vadym.tantsiura@gmail.com>
2025-09-21 15:51:22 +0300

    feat(url-routing-views): create search view and add pagination to game list

--- apps/games/views.py
+++ apps/games/views.py
@@ -13,11 +13,23 @@ def index(request):
 class GameListView(generic.ListView):
     template_name = 'games/game_list.html'
     context_object_name = 'game_list'
-
-    def get_queryset(self):
-        return Game.objects.exclude(title__isnull=True).exclude(title__exact="").order_by('-created_at')
-
+    queryset = Game.objects.exclude(title__isnull=True).exclude(title__exact="").order_by('title')
+    paginate_by = 30
 
 class GameDetailView(generic.DetailView):
     model = Game
     template_name = 'games/game_detail.html'
+
+
+class SearchView(generic.ListView):
+    template_name = 'games/search.html'
+    context_object_name = 'game_list'
+
+
+    def get_queryset(self):
+        query = self.request.GET.get('q')
+        queryset = Game.objects.exclude(title__isnull=True).exclude(title__exact="").order_by('title')
+        if query:
+            return queryset.filter(title__icontains=query)
+        else:
+            return None
